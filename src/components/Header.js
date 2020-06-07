e08b188747f514f4f755c063fc7473e1d8258db2
85rcr <email@rafaelrossini.com>
2020-06-07 16:49:28 -0300

    Continuação do Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import logo from '../svg/logo.svg';
-import { NavLink } from 'react-router-dom';
-import styled from 'styled-components'
+import { NavLink , Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { Icon } from "react-icons-kit";
+import { ic_keyboard_arrow_right } from 'react-icons-kit/md/ic_keyboard_arrow_right'
 
 class Header extends Component {
     render() {
@@ -11,6 +13,15 @@ class Header extends Component {
                     <Logo src={logo} />
                     <NavLink className='signIn-btn' to="/">Sign in</NavLink>
                 </div>
+                {/* Header Content */}
+                <div className="header-content">
+                    <Title>See what's next.</Title>
+                    <SubTitle>Watch anywhere. Cancel anytime.</SubTitle>
+                    <Link className="main-offer-btn">
+                        try it now
+                        <Icon className="Icon" icon={ic_keyboard_arrow_right} size={37} />
+                    </Link>
+                </div>
             </HeaderComponent>
         )
     }
@@ -19,7 +30,6 @@ class Header extends Component {
 export default Header;
 
 // Header Container
-
 const Logo = styled.img`
     width: 10rem;
     height: 3.5rem;
@@ -47,10 +57,66 @@ const HeaderComponent = styled.div`
             background: var(--main-red-hover);
         }
     }
-
+    
+    // Header Top
     .header-top {
         position: relative;
         height: 10rem;
         z-index: 1;
     }
+
+    // Header Content
+    .header-content {
+        width: 65%;
+        position: relative;
+        margin: 4.5rem auto 0;
+        display: flex;
+        justify-content: center;
+        align-content: center;
+        text-align: center;
+        flex-direction: column;
+        z-index: 1;
+    }
+
+    // Main Offer BTN
+    .main-offer-btn {
+        display: inline-block;
+        background: var(--main-red);
+        text-transform: uppercase;
+        border: none;
+        outline: none;
+        margin: 0 33%;
+        padding: 1.5rem;
+        border-radius: 0.1875rem;
+        font-size: 2rem;
+        text-align: center;
+        box-shadow: 0 1px 0 rgba(0,0,0,0.45);
+        transition: background 0.2s ease-in;
+        cursor: pointer;
+        &:hover {
+            background: var(--main-red-hover);
+        }
+    }
+
+    .Icon svg {
+        vertical-align: bottom !important;
+        font-weight: 700;
+    }
+`;
+
+// Main Title
+const Title = styled.h1`
+    margin: 0 0 1.2rem;
+    font-size: 5rem;
+    font-weight: 700;
+    line-heigth: 1.1em;
+`;
+
+// SubTitle
+const SubTitle = styled.h2`
+    font-size: 1.875rem;    
+    font-weight: 400;
+    line-height: 1.25em;
+    margin: 0 0 1.875rem;
+    text-transform: uppercase;
 `;
